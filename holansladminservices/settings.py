65f7d7a0933b5dadeb4286a8b3e4bd47b8cdfb80
AJ Iyanu <aderemijoseph@live.com>
2025-10-13 12:43:57 +0100

    feat: Update build script and settings for Whitenoise integration and static file management

--- holansladminservices/settings.py
+++ holansladminservices/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'holansladminservices.urls'
@@ -172,4 +173,6 @@ SPECTACULAR_SETTINGS = {
 }
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
